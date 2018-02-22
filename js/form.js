beea1bcf54eb1eaed9ed438a6ba0331b636bebdb
shang6723 <31940209+shang6723@users.noreply.github.com>
2018-02-21 20:53:24 -0800

    v1.0.1
    solve overflow bug

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
 var i = 0; // for event id
 
 function form_action_add() {
-    var add_event = "<li id=event" + i + "><label>" + $("#event").val() + "</label>"
+    var add_event = "<li id=event" + i + "><div><label>" + $("#event").val() + "</label></div>"
                 + "<button type='button' onClick='form_action_delete(event"+ i +")'>Delete</button></li>";
     i++;
     $("#list_display").append(add_event);
